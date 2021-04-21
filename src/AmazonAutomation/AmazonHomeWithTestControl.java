644776a0d2173c15bd3bde197387ba2405286e61
jahmed02 <jahmed22b@gmail.com>
2021-04-21 19:00:05 -0400

    manual framework

--- src/AmazonAutomation/AmazonHomeWithTestControl.java
+++ src/AmazonAutomation/AmazonHomeWithTestControl.java
@@ -3,27 +3,41 @@ package AmazonAutomation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class AmazonHomePage {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "J:\\Intellij Projects\\SeleniumDemo\\driver\\chromedriver.exe");
+public class AmazonHomeWithTestControl {
 
-        WebDriver driver = new ChromeDriver();
+    // this way we can test applications using TestNG features. Such as "@" methods
+
+
+    WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "J:\\Intellij Projects\\SeleniumDemo\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.amazon.com/");
         driver.manage().window().maximize();
+    }
+
+
+
+    @Test
+    public void test1() {
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Books" , Keys.ENTER);
         driver.findElement(By.id("twotabsearchtextbox")).clear();
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Honey" , Keys.ENTER);
         driver.findElement(By.id("nav-search-dropdown-card")).click();
+    }
 
-
-        driver.close();   // driver.quit(); will terminate all the open windows
+    @AfterMethod
+    public void cleanUp() {
+        driver.close();
     }
 }
